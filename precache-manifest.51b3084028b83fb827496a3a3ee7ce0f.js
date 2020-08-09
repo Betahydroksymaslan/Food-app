7469765a80760af6ec8520f22e88baa6e9ff413b
Betahydroksymaslan <wieczork@gmail.com>
2020-08-09 22:29:30 +0200

    Updates

---
+++ precache-manifest.51b3084028b83fb827496a3a3ee7ce0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3025a2055c87f85c5e5e879e7947bbbf",
+    "url": "/Food-App/index.html"
+  },
+  {
+    "revision": "b277df3fb0f85aa24e6a",
+    "url": "/Food-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b80f76a5a5828f4015aa",
+    "url": "/Food-App/static/js/2.b60d6eab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-App/static/js/2.b60d6eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b277df3fb0f85aa24e6a",
+    "url": "/Food-App/static/js/main.538d60dd.chunk.js"
+  },
+  {
+    "revision": "4cc8cd69ccd8dd0f9497",
+    "url": "/Food-App/static/js/runtime-main.9843d859.js"
+  }
+]);
